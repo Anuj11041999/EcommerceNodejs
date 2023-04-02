ad86f84ab7c8894d97aa285c2141522c8b1f0ff9
Anuj11041999 <anujchilwery@gmail.com>
2023-04-03 03:22:32 +0530

    server commit

--- first.js
+++ first.js
@@ -1,14 +1,1 @@
-const delay = (s, ms) => new Promise((resolve) => setTimeout(() => {
-    console.log(s);
-    resolve();
-}, ms));
-
-const printInOrder = async () => {
-  console.log('a');
-  console.log('b');
-  await delay('c', 3000);
-  await delay('d', 0);
-  console.log('e');
-};
-
-printInOrder();
+console.log('hello world');
