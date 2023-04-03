95d85b42ce0d7f0860f566c85002465d0f307ec3
Anuj11041999 <anujchilwery@gmail.com>
2023-04-03 19:45:04 +0530

    response commit

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
     console.log(res);
-    console.log('Anuj');
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    res.write('</head>');
+    res.end();
 });
 
-server.listen(4000);
+server.listen(3000);
