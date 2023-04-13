6db62173db3d3c83df569b147334098d3fb5008a
Anuj11041999 <anujchilwery@gmail.com>
2023-04-13 21:27:56 +0530

    relation commit

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('../models/product');
+const User = require('../models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -23,11 +26,24 @@ app.use(contactRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 
 sequelize
+  // .sync({ force: true })
   .sync()
   .then(result => {
+    return User.findById(1);
     // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch(err => {
